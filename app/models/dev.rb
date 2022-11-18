8e3ff52abebafd09fe19a21a8181f8604432e2b8
Elias <eliasventura23@gmail.com>
2022-11-18 09:09:10 -0500

    finished lab

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one(item_name)
+        self.freebies.find_by(item_name: item_name) ? true : false
+    end
+
+    def give_away(dev, freebie)
+       if self.received_one(freebie.item_name)
+        freebie.dev = dev
+        freebie.dev
+       end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,12 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "bookbag", value: 5, company_id: 1, dev_id: 4)
+Freebie.create(item_name: "bookbag", value: 4, company_id: 4, dev_id: 1)
+Freebie.create(item_name: "bookbag", value: 8, company_id: 2, dev_id: 3)
+Freebie.create(item_name: "bookbag", value: 1, company_id: 3, dev_id: 2)
+Freebie.create(item_name: "bookbag", value: 2, company_id: 1, dev_id: 4)
+
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
